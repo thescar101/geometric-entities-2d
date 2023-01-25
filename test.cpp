e8128bfa13f898eb9f3f9d594492748e02c3a7b5
Oscar Rodas <60824325+thescar101@users.noreply.github.com>
2023-01-24 21:14:54 -0500

    Testing rational number

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "entities.h"
+#include <gmp.h>
 using namespace std;
 
 int main()
 {
 	cout << "Test successful." << endl;
+	mpq_t number;
+	mpq_init(number); // Intialize to 0/1
+	mpq_set_si(number, 2, 1); // Set to 2/1
+	cout << number;
 	return 0;
 }
